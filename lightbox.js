847decbd3445cd685c388e635c383ce2e157dfa3
Luisgarcet7 <109825022+Luisgarcet7@users.noreply.github.com>
2022-07-22 16:11:05 -0400

    Add files via upload

---
+++ lightbox.js
@@ -0,0 +1,15 @@
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenesLight = document.querySelector('.agregar-imagen')
+const contenedorLight = document.querySelector('.imagen-light')
+
+imagenes.forEach(imagen =>{
+  imagen.addEventListener('click', ()=>{
+    
+  })
+  
+})
+
+const aparecerImagen = (imagen)=>{
+  
+  
+}
